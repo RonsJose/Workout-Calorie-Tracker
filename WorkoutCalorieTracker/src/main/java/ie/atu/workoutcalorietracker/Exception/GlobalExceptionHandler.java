0e0ab029fb9c3992afc51ed61a6acea2894c4c96
RonsJose <g00464215@atu.ie>
2026-02-23 11:51:31 +0000

    Added validation and error handling

---
+++ WorkoutCalorieTracker/src/main/java/ie/atu/workoutcalorietracker/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package ie.atu.workoutcalorietracker.Exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        }
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
