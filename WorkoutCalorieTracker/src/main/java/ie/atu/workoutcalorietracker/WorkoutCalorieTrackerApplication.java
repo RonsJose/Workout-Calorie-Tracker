9ceed569ecfd0e0978e91e80c001d509083c37a8
RonsJose <g00464215@atu.ie>
2026-02-23 11:41:45 +0000

    Initial Commit

---
+++ WorkoutCalorieTracker/src/main/java/ie/atu/workoutcalorietracker/WorkoutCalorieTrackerApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.workoutcalorietracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutCalorieTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutCalorieTrackerApplication.class, args);
+    }
+
+}
---
+++ WorkoutCalorieTracker/src/test/java/ie/atu/workoutcalorietracker/WorkoutCalorieTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.workoutcalorietracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutCalorieTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
