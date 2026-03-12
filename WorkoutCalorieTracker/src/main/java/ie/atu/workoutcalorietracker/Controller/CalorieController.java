8931b8ff269d0b60234b8661a27add43983fd0d9
SuperButler <g00467621@atu.ie>
2026-03-12 16:31:44 +0000

    created and added Calorie Goals

--- WorkoutCalorieTracker/src/main/java/ie/atu/workoutcalorietracker/Controller/CalorieController.java
+++ WorkoutCalorieTracker/src/main/java/ie/atu/workoutcalorietracker/Controller/CalorieController.java
@@ -1,6 +1,38 @@
 package ie.atu.workoutcalorietracker.Controller;
 
+import ie.atu.workoutcalorietracker.Model.Goal;
+import ie.atu.workoutcalorietracker.Services.CalorieService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/calories")
 public class CalorieController {
-    /// Option to view goal (Goal)
-    /// Option to add food calories (Food)
+
+    private final CalorieService calorieService;
+
+    public CalorieController(CalorieService calorieService) {
+        this.calorieService = calorieService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Goal> createGoal(@Valid @RequestBody Goal goal) {
+        Goal newGoal = calorieService.createGoal(goal);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newGoal);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Goal>> getAll() {
+        return ResponseEntity.ok(calorieService.getAllGoals());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Goal> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(calorieService.getGoalById(id));
+    }
+
 }
--- WorkoutCalorieTracker/src/main/java/ie/atu/workoutcalorietracker/Model/Goal.java
+++ WorkoutCalorieTracker/src/main/java/ie/atu/workoutcalorietracker/Model/Goal.java
@@ -1,7 +1,26 @@
 package ie.atu.workoutcalorietracker.Model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class Goal {
-    /// Allow user to add goal
-    /// Allow user to view goal
-    /// Show if user has reached day / week / month goal
+
+    private Long id;
+
+    @NotNull(message = "Goal must not me empty")
+    @Min(value = 1, message = "Goal cant be less than 0")
+    private Long calorieGoal;
+
+    @NotBlank(message = "Must enter gain or loss")
+    private String gainLoss;
+
+    @NotNull(message = "Must enter a date")
+    private LocalDate goalDate;
 }
