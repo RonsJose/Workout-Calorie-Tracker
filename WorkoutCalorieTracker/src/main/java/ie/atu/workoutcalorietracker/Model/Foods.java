71d463cafaf0e5e800d819aab0fcab635378060d
RonsJose <g00464215@atu.ie>
2026-04-04 17:36:05 +0100

    Removed goal microservice additions

--- WorkoutCalorieTracker/src/main/java/ie/atu/workoutcalorietracker/Model/Foods.java
+++
@@ -1,7 +0,0 @@
-package ie.atu.workoutcalorietracker.Model;
-
-public class Foods {
-    /// Allow user to add food they have eaten (send to controller)
-    /// Allow user to plan diet
-    /// Allow user to add own calories if not in given list
-}
