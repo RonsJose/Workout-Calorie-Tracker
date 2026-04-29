70ae90876b551bcc6a2879fac51b9f279f072d66
RonsJose <g00464215@atu.ie>
2026-04-29 14:36:07 +0100

    Revert "Added my files from my outher repo, didnt realise when he said each service must use a diffrent repo it was for the code not the git..."
    This reverts commit bbed4e2ba1ed0bd28024bcebc8370cf772e3b6b4.

--- WorkoutCalorieTracker/src/main/java/ie/atu/workoutcalorietracker/repository/IntakeRepo.java
+++
@@ -1,8 +0,0 @@
-package ie.atu.workoutcalorietracker.repository;
-
-import ie.atu.workoutcalorietracker.Model.Intake;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IntakeRepo extends JpaRepository<Intake, Long> {
-}
-
